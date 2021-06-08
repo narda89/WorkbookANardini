58b3c96039bb5485411b1c83a5024569f1daf5d9
narda89 <nardanarda89@gmail.com>
2021-06-08 18:20:06 +0200

    Inseriti secondo e terzo ciclo. Versioni provvisorie. Manco io ho capito perché funzionano, ho sbatacchiato i tasti a caso finché non andava

--- src/ires/corso/partone/WB210608/LoopTest.java
+++ src/ires/corso/partone/WB210608/LoopTest.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class LoopTest {
 
-    public static void main (String[] args) {
+    public static void TestLoopOne() {
         int i, j, n;
         int m = 1;
         System.out.print("Per quanto vuoi iterare? ");
@@ -20,6 +20,106 @@ public class LoopTest {
             }
             System.out.println("");
         }
+
+    }
+
+    public static void TestLoopTwo() {
+        int i, j, k, n;
+
+        System.out.print("Dimensioni? ");
+        Scanner theScan = new Scanner(System.in);
+        n = theScan.nextInt();
+        int size = (n * 2) - 1;
+
+
+        //Stampa meta superiore diamante
+        for (i = 0; i < n; i++ )
+        {
+            //stampa gli spazi
+            for ( j = 0; j <= (size / 2) - i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            // stampa *
+            for ( k = 0; k < ((i+1) * 2) - 1; k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        //stampa meta inferiore diamante
+        for (i = n ; i  >= 0; i-- )
+        {
+            //stampa gli spazi
+            for ( j = (size / 2) - i; j >= 0; j--)
+            {
+                System.out.print(" ");
+            }
+
+            // stampa *
+            for ( k = ((i+1) * 2) - 1; k > 0; k--)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+
+    // nb: funzione chiara tanto quanto l'aramaico antico. E non parlo aramaico. Però funziona e bon
+
+    public static void TestLoopThree() {
+        int i, j, k, n;
+
+        System.out.print("Dimensioni? ");
+        Scanner theScan = new Scanner(System.in);
+        n = theScan.nextInt();
+        int size = (n * 2) - 1;
+        int into;
+
+
+        //Stampa meta superiore diamante
+        for (i = 0; i < n; i++ )
+        {
+            //stampa gli spazi
+            for ( j = 0; j <= (size / 2) - i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            // stampa *
+            for ( k = 0, into = i; k < ((i+1) * 2) - 1; k++, into--)
+            {
+                System.out.print(Math.abs(into)+1);
+            }
+            System.out.println("");
+        }
+
+        //stampa meta inferiore diamante
+        for (i = n ; i  >= 0; i-- )
+        {
+            //stampa gli spazi
+            for ( j = (size / 2) - i; j >= 0; j--)
+            {
+                System.out.print(" ");
+            }
+
+            // stampa *
+            for ( k = ((i+1) * 2) - 1, into = i; k > 0; k--, into-- )
+            {
+                System.out.print(Math.abs(into)+1);
+            }
+            System.out.println("");
+        }
+
+    }
+
+    public static void main (String[] args) {
+        TestLoopOne();
+        TestLoopTwo();
+        TestLoopThree();
 
     }
 }
