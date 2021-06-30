031ad5995c2bcc1066f8bdf3c05df118ac296768
narda89 <nardanarda89@gmail.com>
2021-06-30 15:36:47 +0200

    todo fourth commit - works (kinda), added methods in ToDoList

--- src/ires/corso/parttwo/todo/ToDoList.java
+++ src/ires/corso/parttwo/todo/ToDoList.java
@@ -3,9 +3,7 @@ package ires.corso.parttwo.todo;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 // GRUPPO 1:
 public class ToDoList
@@ -15,14 +13,14 @@ public class ToDoList
         String choice = null;
         do {
             System.out.println( "---------------------------------------------------\n" +
-                    "|                ToDo List/Show menu               |\n" +
-                    "---------------------------------------------------\n");
+                                "|                ToDo List/Show menu               |\n" +
+                                "---------------------------------------------------\n");
 
             Scanner scan = new Scanner(System.in);
 
             System.out.printf(  "[1] Show all data\n" +
-                    "[2] Show by...\n" +
-                    "[3] Back\n");
+                                "[2] Show by...\n" +
+                                "[3] Back\n");
 
             choice = scan.nextLine();
 
@@ -45,6 +43,31 @@ public class ToDoList
             entry.getValue().prettyPrint();
     }
 
+    public static void viewByID() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Insert corresponding ID: ");
+        try {
+            ToDoRepository.get_data().get(scan.nextInt()).prettyPrint();
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please insert an integer/ insert an existing ID");
+        }
+    }
+
+    public static void viewByAscendindDueDate() {
+        Date[] dates = new Date[ToDoRepository.repoSize()];
+        HashMap<Date, ToDo> toBeOrdered = new HashMap<>();
+        int j = 0;
+        for (Map.Entry<Integer, ToDo> map : ToDoRepository.get_data().entrySet()) {
+            dates[j] = new Date(map.getValue().getEndDate().getTime());
+            toBeOrdered.put(dates[j], map.getValue());
+            j++;
+        }
+        Arrays.sort(dates);
+
+        for(Date d: dates)
+            toBeOrdered.get(d).prettyPrint();
+    }
+
     static void showBySubMenu() throws ParseException {
         String choice = null;
         do {
@@ -52,15 +75,18 @@ public class ToDoList
 
             Scanner scan = new Scanner(System.in);
 
-            System.out.printf(  "[1] Priority\n"  +
-                    "[2] Status\n" +
-                    "[3] Due date\n"    +
-                    "[4] Back\n");
+            System.out.printf(  "[1] Priority\n"    +
+                                "[2] Status\n"      +
+                                "[3] Due date\n"    +
+                                "[4] Specific ID\n" +
+                                "[5] Ascending due date\n"+
+                                "[6] Back\n");
 
             choice = scan.nextLine();
 
-            if (choice.equals("4"))
+            if (choice.equals("6"))
                 return;
+
             else if (choice.equals("1")) {
                 // show by priority
                 System.out.printf("Specify priority:");
@@ -69,6 +95,7 @@ public class ToDoList
                 } catch (Exception e) {
                     System.out.println("Invalid input: cannot recognize valid priority (S, A, B, C, D)");
                 }
+
             } else if (choice.equals("2")) {
                 // show by status
                 System.out.printf("Specify status:");
@@ -79,7 +106,7 @@ public class ToDoList
                             "COMPLETATO, ANNULLATO)");
                 }
 
-            } else if (choice.equals("3")) {
+            } else if (choice.equals("3")){
                 // show by due date
                 System.out.printf("Specify date:");
                 String sData = scan.nextLine();
@@ -89,10 +116,19 @@ public class ToDoList
                 } catch (Exception e) {
                     System.out.println("Invalid input: cannot recognize date in dd/mm/yyyy format.");
                 }
+
+            } else if (choice.equals("4")){
+                // show by specific ID
+                viewByID();
+
+            } else if (choice.equals("5")){
+                // show by due date, ascending order (still buggy)
+                viewByAscendindDueDate();
+
             } else
                 System.out.println("Invalid input. Please insert valid input.");
 
-        } while(!(choice.equals("4")));
+        } while(!(choice.equals("6")));
     }
 
     public static void viewByEndDate(Date d) {
